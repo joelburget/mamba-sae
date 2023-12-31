123efcaee17cc6aa0283ece8a892ccbab3f2cd81
Joel Burget <joelburget@gmail.com>
2023-12-30 17:05:26 -0800

    Analyze all features at once.

--- analyze_sae.py
+++ analyze_sae.py
@@ -20,34 +20,44 @@ ae = AutoEncoder(activation_dim, dictionary_size)
 ae.load_state_dict(state_dict)
 
 
-def activation_on_input(feature_no: int, input: str) -> float:
+# TODO: do a whole batch at a time
+def activations_on_input(input: str) -> torch.Tensor:
     tokens = tokenizer(input)['input_ids']
     with model.invoke(tokens) as _invoker:
         embedding = model.model.embedding
         intervention_proxy = embedding.output[0].save()
-    return ae.encode(intervention_proxy.value.cpu())[0, feature_no].item()
+    return ae.encode(intervention_proxy.value.cpu())[0]
 
 
 @dataclass
 class AnalysisResult:
-    max_activations: List[Tuple[float, str]]
+    max_activations: List[List[Tuple[float, str]]]
     # TODO: logits, histogram
 
 
-def analyze_feature(feature_no: int, n: int) -> AnalysisResult:
-    min_heap = []
+def analyze_features(n: int) -> AnalysisResult:
+    min_heaps = [[] for _ in range(dictionary_size)]
 
     for example in tqdm(short_data):
-        activation = activation_on_input(feature_no, example)
+        activations = activations_on_input(example)
 
-        if len(min_heap) < n:
-            heapq.heappush(min_heap, (-activation, example))
-        else:
-            heapq.heappushpop(min_heap, (-activation, example))
+        for feature_n in range(dictionary_size):
+            min_heap = min_heaps[feature_n]
+            activation = activations[feature_n].item()
+            if len(min_heap) < n:
+                heapq.heappush(min_heap, (-activation, example))
+            else:
+                heapq.heappushpop(min_heap, (-activation, example))
 
-    return AnalysisResult([(-neg_activation, example)
-                           for neg_activation, example in min_heap])
+    return AnalysisResult([[(-neg_activation, example)
+                            for neg_activation, example in min_heap]
+                           for min_heap in min_heaps])
+
+
+def print_top_acts(acts: List[Tuple[float, str]], excerpt_length=200):
+    for score, text in acts:
+        print(f'\033[1m{score}\033[0m', text[:excerpt_length])
 
 
 if __name__ == '__main__':
-    print(analyze_feature(0, 6))
+    print(analyze_features(6))
