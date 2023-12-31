90983dedb10b97c9186f4d9a4815887890d5a4ca
Joel Burget <joelburget@gmail.com>
2023-12-30 17:05:07 -0800

    Much cleaner tokenizer implementation.

--- tokenizer.py
+++ tokenizer.py
@@ -9,48 +9,20 @@ class Tokenizer:
         self.wrapped.eos_token = "<|endoftext|>"
         self.wrapped.pad_token = self.wrapped.eos_token
 
+    def __getattr__(self, name):
+        """
+        Delegate to the wrapped object for any method or attribute not defined in this class.
+        """
+
+        def method(*args, **kwargs):
+            result = getattr(self.wrapped, name)(*args, **kwargs)
+            if name == 'pad':
+                del result['attention_mask']
+            return result
+
+        return method
+
     def __call__(self, *args, **kwargs):
         result = self.wrapped(*args, **kwargs)
-        del result['attention_mask']
-        return result
-
-    def add_tokens(self, *args, **kwargs):
-        return self.wrapped.add_tokens(*args, **kwargs)
-
-    def add_special_tokens(self, *args, **kwargs):
-        return self.wrapped.add_special_tokens(*args, **kwargs)
-
-    def apply_chat_template(self, *args, **kwargs):
-        return self.wrapped.apply_chat_template(*args, **kwargs)
-
-    def batch_decode(self, *args, **kwargs):
-        return self.wrapped.batch_decode(*args, **kwargs)
-
-    def decode(self, *args, **kwargs):
-        return self.wrapped.decode(*args, **kwargs)
-
-    def encode(self, *args, **kwargs):
-        return self.wrapped.encode(*args, **kwargs)
-
-    def convert_ids_to_tokens(self, *args, **kwargs):
-        return self.wrapped.convert_ids_to_tokens(*args, **kwargs)
-
-    def convert_tokens_to_ids(self, *args, **kwargs):
-        return self.wrapped.convert_tokens_to_ids(*args, **kwargs)
-
-    def get_added_vocab(self, *args, **kwargs):
-        return self.wrapped.get_added_vocab(*args, **kwargs)
-
-    def num_special_tokens_to_add(self, *args, **kwargs):
-        return self.wrapped.num_special_tokens_to_add(*args, **kwargs)
-
-    def set_truncation_and_padding(self, *args, **kwargs):
-        return self.wrapped.set_truncation_and_padding(*args, **kwargs)
-
-    def train_new_from_iterator(self, *args, **kwargs):
-        return self.wrapped.train_new_from_iterator(*args, **kwargs)
-
-    def pad(self, *args, **kwargs):
-        result = self.wrapped.pad(*args, **kwargs)
         del result['attention_mask']
         return result
