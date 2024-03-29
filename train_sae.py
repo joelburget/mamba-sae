64a60d35192ebdb315f5879f1c520ef71e7dc2a6
Joel Burget <joelburget@gmail.com>
2024-03-29 14:51:07 -0700

    Migrate train_sae to sae_training library.

--- train_sae.py
+++ train_sae.py
@@ -5,9 +5,8 @@ from datasets import load_dataset
 from tqdm import tqdm
 from transformers import MambaForCausalLM
 
-from dictionary_learning.buffer import ActivationBuffer
-from dictionary_learning.training import trainSAE
-from dictionary_learning.evaluation import evaluate
+from sae_training.config import LanguageModelSAERunnerConfig
+from sae_training.lm_runner import language_model_sae_runner
 
 from params import (
     d_model,
@@ -22,7 +21,6 @@ from params import (
 if __name__ == "__main__":
     dataset = load_dataset(dataset_path, split="train", streaming=True)
     model = MambaForCausalLM.from_pretrained(model_path)
-    lr = 3e-4
     if not os.path.exists(sae_dir):
         os.makedirs(sae_dir)
 
@@ -32,26 +30,36 @@ if __name__ == "__main__":
         for relative_size in tqdm(
             relative_sizes, desc="relative_size", position=1, leave=False
         ):
-            dictionary_size = relative_size * d_model
-            submodule = model.model.layers[0]
-
-            buffer = ActivationBuffer(
-                data=(example["text"] for example in dataset.take(200_000)),
-                model=model,
-                submodule=submodule,
-                in_feats=d_model,
-                out_feats=d_model,
+            cfg = LanguageModelSAERunnerConfig(
+                model_name=f"mamba-1l-{relative_size}-{sparsity_penalty}",
+                hook_point="layers.0.hook_resid_pre",
+                hook_point_layer=0,
+                d_in=d_model,
+                dataset_path="Skylion007/openwebtext",
+                is_dataset_tokenized=False,
+                # SAE Parameters
+                expansion_factor=relative_size,
+                b_dec_init_method="geometric_median",
+                # Training Parameters
+                l1_coefficient=sparsity_penalty,
+                context_size=128,
+                lr_warm_up_steps=5000,
+                # Activation Store Parameters
+                n_batches_in_buffer=128,
+                total_training_tokens=1_000_000 * 300,
+                store_batch_size=32,
+                # Dead Neurons and Sparsity
+                use_ghost_grads=True,
+                feature_sampling_window=1000,
+                dead_feature_window=5000,
+                dead_feature_threshold=1e-6,
+                # WANDB
+                log_to_wandb=True,
+                wandb_project="mamba-sae",
+                wandb_log_frequency=100,
+                # Misc
+                device="cuda",
+                n_checkpoints=10,
             )
-
-            ae = trainSAE(
-                activations=buffer,
-                activation_dim=d_model,
-                dictionary_size=dictionary_size,
-                lr=lr,
-                sparsity_penalty=sparsity_penalty,
-                device="cuda:0",
-            )
-
-            print(evaluate(model, submodule, ae, buffer))
-
-            torch.save(ae.state_dict(), sae_path)
+            sae = language_model_sae_runner(cfg)
+            torch.save(sae.state_dict(), sae_path)
